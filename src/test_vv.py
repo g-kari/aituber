e6ec8bd4a6e091cb885d5af85dc0b903f650ba1a
g.k <gizensya.mail@gmail.com>
2024-03-29 12:33:53 +0900

    Add .vscode to .gitignore and add requirements.txt***
    ***Delete activate.bat, deactivate.bat, and sound_device.txt***
    
    ***Add test_vv.py

--- src/test_vv.py
+++
@@ -1,9 +0,0 @@
-from voicevox_adapter import VoicevoxAdapter
-from play_sound import PlaySound
-
-input_text = input("こんにちは")
-vv_adater = VoicevoxAdapter()
-play_sound = PlaySound()
-data, rate = vv_adater.get_voice(input_text)
-
-play_sound.play_scoud(data, rate)
